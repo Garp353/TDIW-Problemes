738c16bd87d9d791244c2f4b1d81816520d42921
Victor <vfraucam@gmail.com>
2025-10-08 18:38:14 +0200

    P3

--- mencions.php
+++ mencions.php
@@ -1,16 +1,36 @@
 <?php
   $graus = array(
     array("id"=>1,"nom"=>"Eng. Informàtica"), 
-    array("id"=>2,"nom"=>"Intel.ligència Artificial")
+    array("id"=>2,"nom"=>"Intel.ligència Artificial"),
+    array("id"=>3,"nom"=>"Telecos de Sistemes"),
+    array("id"=>4,"nom"=>"Telecos d'Electrònica"),
+    array("id"=>5,"nom"=>"Enginyeria Química")
   );
   $mencions = array( 
     1=> array(
-      array("id"=>1,"nom"=>"Eng Software"), 
-      array("id"=>2,"nom"=>"TIC"),
-      array("id"=>3,"nom"=>"Computació")),
-    2=> array(   
-      array("id"=>4,"grau_id"=>2,"nom"=>"Machine Learning"),
-      array("id"=>5,"grau_id"=>2,"nom"=>"Data mining"))
+      array("id"=>1,"nom"=>"Enginyeria del software"), 
+      array("id"=>2,"nom"=>"Enginyeria de computadors"),
+      array("id"=>3,"nom"=>"Computació"),
+      array("id"=>4,"nom"=>"Tecnologies de la informació")
+    ),
+    2=> array(
+      array("id"=>5,"nom"=>"Sistemes Analogics"),
+      array("id"=>6,"nom"=>"Sistemes digitals"),
+      array("id"=>7,"nom"=>"Xarxes de telecomunicacions"),
+      array("id"=>8,"nom"=>"Processament de senyals")
+    ),
+    3=> array(
+      array("id"=>9,"nom"=>"Electrònica analògica"),
+      array("id"=>10,"nom"=>"Microelectrònica"),
+      array("id"=>11,"nom"=>"Dispositius electrònics"),
+      array("id"=>12,"nom"=>"Circuits integrats")
+    ),
+    4=> array(
+      array("id"=>13,"nom"=>"Processos químics"),
+      array("id"=>14,"nom"=>"Enginyeria de reactors"),
+      array("id"=>15,"nom"=>"Operacions unitàries"),
+      array("id"=>16,"nom"=>"Control de processos")
+    )
   );
 
   $grau = $_REQUEST['grau'];
